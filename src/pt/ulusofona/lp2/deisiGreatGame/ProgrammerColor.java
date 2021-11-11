87ee0f2c8e29ee69ba16beeb585638a9d7766b8a
Simão Bento <a22003314@alunos.ulht.pt>
2021-11-11 22:37:21 +0000

    Update ao ProgrammerColor

--- src/pt/ulusofona/lp2/deisiGreatGame/ProgrammerColor.java
+++ src/pt/ulusofona/lp2/deisiGreatGame/ProgrammerColor.java
@@ -1,14 +1,9 @@
 package pt.ulusofona.lp2.deisiGreatGame;
 
 enum ProgrammerColor {
-    PURPLE("Purple"),
-    BLUE("Blue"),
-    GREEN("Green"),
-    BROWN("Brown");
-    String color;
-
-    ProgrammerColor(String color) {
-        this.color = color;
-    }
+    PURPLE,
+    BLUE,
+    GREEN,
+    BROWN
 }
 
