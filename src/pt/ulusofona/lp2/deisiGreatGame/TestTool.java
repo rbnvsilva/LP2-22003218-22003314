49e20fd7ad2265fe134d898789a55771ee7eb118
Ruben Silva <a22003218@alunos.ulht.pt>
2021-12-12 20:48:19 +0000

    correcao de um bug

--- src/pt/ulusofona/lp2/deisiGreatGame/TestTool.java
+++ src/pt/ulusofona/lp2/deisiGreatGame/TestTool.java
@@ -64,26 +64,26 @@ public class TestTool {
         abyssesAndTools[0][2] = "3";
         gameManager.createInitialBoard(playerInfo, 24, abyssesAndTools);
         gameManager.getAbyssesOrTools().get(0).message();
-        assertEquals("Herança\nTens mesmo sorte! Imune ao abismo Efeitos Secundários +1 vez.", gameManager.getAbyssesOrTools().get(0).message());
+        assertEquals("Herança\nTens mesmo sorte! Imune ao abismo Duplicated Code 1 vez.", gameManager.getAbyssesOrTools().get(0).message());
         abyssesAndTools[0][1] = "1";
         gameManager.createInitialBoard(playerInfo, 24, abyssesAndTools);
         gameManager.getAbyssesOrTools().get(0).message();
-        assertEquals("Programação funcional\nTens mesmo sorte! Imune aos seguintes abismos +1 vez:\nCiclo Infinito e Segmentation Fault.", gameManager.getAbyssesOrTools().get(0).message());
+        assertEquals("Programação funcional\nTens mesmo sorte! Imune aos seguintes abismos 1 vez:\nCiclo Infinito e Efeitos secundários.", gameManager.getAbyssesOrTools().get(0).message());
         abyssesAndTools[0][1] = "2";
         gameManager.createInitialBoard(playerInfo, 24, abyssesAndTools);
         gameManager.getAbyssesOrTools().get(0).message();
-        assertEquals("Testes unitários\nTens mesmo sorte! Imune aos seguintes abismos +1 vez:\nCrash (aka Rebentanco).", gameManager.getAbyssesOrTools().get(0).message());
+        assertEquals("Testes unitários\nTens mesmo sorte! Imune aos seguintes abismos 1 vez:\nErro de lógica.", gameManager.getAbyssesOrTools().get(0).message());
         abyssesAndTools[0][1] = "3";
         gameManager.createInitialBoard(playerInfo, 24, abyssesAndTools);
         gameManager.getAbyssesOrTools().get(0).message();
-        assertEquals("Tratamento de Excepções\nTens mesmo sorte! Imune aos seguintes abismos +1 vez:\nException e File Not Found Exception.", gameManager.getAbyssesOrTools().get(0).message());
+        assertEquals("Tratamento de Excepções\nTens mesmo sorte! Imune aos seguintes abismos 1 vez:\nException e File Not Found Exception.", gameManager.getAbyssesOrTools().get(0).message());
         abyssesAndTools[0][1] = "4";
         gameManager.createInitialBoard(playerInfo, 24, abyssesAndTools);
         gameManager.getAbyssesOrTools().get(0).message();
-        assertEquals("IDE\nTens mesmo sorte! Imune aos seguintes abismos +1 vez:\nDuplicated Code.", gameManager.getAbyssesOrTools().get(0).message());
+        assertEquals("IDE\nTens mesmo sorte! Imune aos seguintes abismos 1 vez:\nErro de sintaxe.", gameManager.getAbyssesOrTools().get(0).message());
         abyssesAndTools[0][1] = "5";
         gameManager.createInitialBoard(playerInfo, 24, abyssesAndTools);
         gameManager.getAbyssesOrTools().get(0).message();
-        assertEquals("Ajuda Do Professor\nTens mesmo sorte! Imune aos seguintes abismos +1 vez:\nErro de sintaxe, Erro de logica, Exception, File Not Found Exception.", gameManager.getAbyssesOrTools().get(0).message());
+        assertEquals("Ajuda Do Professor\nTens mesmo sorte! Imune aos seguintes abismos 1 vez:\nErro de sintaxe, Erro de lógica, Exception, File Not Found Exception.", gameManager.getAbyssesOrTools().get(0).message());
     }
 }
